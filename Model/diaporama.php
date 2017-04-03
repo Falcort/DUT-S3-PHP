1c2926f9a8643e165ca8491725a9834e2cf525e3
Falcort <falcort.jeux@gmail.com>
2017-04-03 08:14:07 +0200

    Gitignore et OrderBy

--- Model/diaporama.php
+++ Model/diaporama.php
@@ -2,6 +2,6 @@
 
 $bdd = new bdd ($MYSQL_host, $MYSQL_dbname, $MYSQL_user, $MYSQL_password);
 
-$monquerry = "SELECT * FROM image, image_description WHERE image.id = image_description.id_image";
+$monquerry = "SELECT * FROM image, image_description WHERE image.id = image_description.id_image ORDER BY ordre";
 $bdd->querryArray($monquerry);
 $diapositives = $bdd->donnees;
